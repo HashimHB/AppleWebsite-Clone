f17d76afa24b51dd6267ae87244993b35b15377f
HashimHB <78270317+HashimHB@users.noreply.github.com>
2024-01-22 21:33:42 +0530

    "Initial Commit"

---
+++ script.js
@@ -0,0 +1,77 @@
+// function loco(){
+//     gsap.registerPlugin(ScrollTrigger);
+
+// // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+// const locoScroll = new LocomotiveScroll({
+//   el: document.querySelector("body"),
+//   smooth: true
+// });
+// // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+// locoScroll.on("scroll", ScrollTrigger.update);
+
+// // tell ScrollTrigger to use these proxy methods for the "body" element since Locomotive Scroll is hijacking things
+// ScrollTrigger.scrollerProxy("body", {
+//   scrollTop(value) {
+//     return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+//   }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+//   getBoundingClientRect() {
+//     return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+//   },
+//   // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+//   pinType: document.querySelector("body").style.transform ? "transform" : "fixed"
+// });
+
+// // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+// ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+// ScrollTrigger.refresh();
+
+// }
+// loco()
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "1.2",
+  centeredSlides: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+      keyboard: true,
+      loop:true
+});
+
+
+var menu = document.querySelector(".ri-menu-line");
+var close = document.querySelector(".ri-close-line");
+var navbar = document.querySelector(".subnav");
+
+menu.addEventListener("click",function(){
+  navbar.style.top="0%";
+})
+
+close.addEventListener("click",function(){
+  navbar.style.top="-109%";
+})
+
+
+var Store = document.querySelector("#Store");
+var hovermenu = document.querySelector(".nav3");
+
+Store.addEventListener("mousemove",function(){
+  hovermenu.style.top="5%";
+})
+
+Store.addEventListener("mouseleave",function(){
+  hovermenu.style.top="-100%";
+})
